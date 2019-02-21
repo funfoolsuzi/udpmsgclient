645b7c58df316cde11c31919bf2ec7a28a16cc06
Xiwen <funfoolsuzi@gmail.com>
2019-02-20 22:24:19 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# UDP Message Client
+
+a practice using Rust to build duo-thread udp client that can send and receive messages
+
+### Build
+```
+make build
+```
+
+### Use
+```
+./udpmsgclient {origin_port} {destination_address}
+```
+
+### TODOs
+
+1. use ncurses to build a UI
+2. change destination address in-app
+3. consider using [pnet](https://github.com/libpnet/libpnet) lib
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::net::{UdpSocket};
+use std::io::{self, BufRead};
+use std::env;
+use std::thread;
+use std::sync::Arc;
+use std::str;
+
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let origin = "127.0.0.1:".to_owned() + &args[1];
+    let destination = &args[2];
+
+    println!("starting chat between 127.0.0.1:{} and {}", origin, destination);
+    
+    chat(&origin, destination);
+}
+
+fn chat(origin: &String, destination: &String) {
+    let socket = Arc::new(UdpSocket::bind(origin).expect(&*format!("Unable to bind to {}", origin)));
+
+    let socket_sender = socket.clone();
+
+    thread::spawn(move || {
+        loop {
+            let mut buf = [0; 1024];
+            let (_, src_addr) = socket_sender.recv_from(&mut buf).unwrap();
+            let msg = str::from_utf8(&buf).unwrap();
+            println!("{}: {}", src_addr, msg);
+        }
+    });
+
+    let stdin = io::stdin();
+
+    for line_res in stdin.lock().lines() {
+
+        let line = line_res.unwrap();
+        println!("sending {}", line);
+        socket.send_to((&line[..]).as_bytes(), destination).unwrap();
+
+    }
+}
